9bb42327908e797950577c391631a8b2f51e42c2
Aaryan <102778679+Aaryan-02@users.noreply.github.com>
2024-11-16 19:34:52 +0530

    Added Loader for landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Suspense, lazy, useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import loader from "./assets/loader.gif";
+import styled from 'styled-components';
 
 // Code-Splitting using React Lazy
 const Chat = lazy(() => import("./pages/Chat"));
@@ -33,11 +35,17 @@ const App = () => {
     wakeBackend();
   }, []);
 
-  if (loading) return <div>Loading...</div>;
+  if (loading) return <Container>
+          <img src={loader} alt="loader" className='loader' />
+        </Container>;
 
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={
+        <Container>
+          <img src={loader} alt="loader" className='loader' />
+        </Container>
+      }>
         <Routes>
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
@@ -50,3 +58,61 @@ const App = () => {
 };
 
 export default App;
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 3rem;
+    background-color: #131324;
+    height: 100vh;
+    width: 100vw;
+    .loader {
+        max-inline-size: 100%;
+    }
+      
+    .title-container {
+        h1 {
+            color: white;
+        }
+    }
+      
+  .avatars {
+    display: flex;
+    gap: 2rem;
+      
+        .avatar {
+            border: 0.4rem solid transparent;
+            padding: 0.4rem;
+            border-radius: 5rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transition: 0.5s ease-in-out;
+            img {
+                height: 6rem;
+                transition: 0.5s ease-in-out;
+            }
+        }
+        .selected {
+            border: 0.4rem solid #4e0eff;
+        }
+    }
+    .submit-btn {
+        background-color: #4e0eff;
+        color: white;
+        padding: 1rem 2rem;
+        border: none;
+        font-weight: bold;
+        cursor: pointer;
+        border-radius: 0.4rem;
+        font-size: 1rem;
+        text-transform: uppercase;
+        &:hover {
+            background-color: #4e0eff;
+        }
+    }
+`;
+
+
